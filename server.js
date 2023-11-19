a44564add990c122197c8d1012e4cc5e506846c7
Sasikalamurugesan <118598868+Sasikalamurugesan@users.noreply.github.com>
2023-11-19 14:22:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 mongoose.connect('mongodb+srv://sasikalam21it:Gd4CeQAoYntoLIBi@cluster0.2tw6imi.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -13,7 +13,7 @@ mongoose.connect('mongodb+srv://sasikalam21it:Gd4CeQAoYntoLIBi@cluster0.2tw6imi.
 app.use(bodyParser.json());
 const cors = require('cors');
 const corsOptions = {
-  origin: 'http://localhost:3000', 
+  origin: 'http://localhost:10000', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
